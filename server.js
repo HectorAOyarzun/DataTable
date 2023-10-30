8d87228bf5f63689afbe8ea50f03b4b09282a05d
HectorAOyarzun <hector.oyarzun.mu@gmail.com>
2023-10-30 01:40:48 -0300

    [2do push]readme

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ function doGet() {
   }
   
   function getData(){
-    const base = '1hrYDUmyzz-5MennkLmaj1IW510mS0LjSchgDuBJD0IM';
+    const base = '1YX51IOq0D2iBFsLNJwP4cUThgzIf3b33gruI4aTq2QA';
     const ss = SpreadsheetApp.openById(base);
     const sheet = ss.getSheetByName('0');
     const [headers, ...usersInfo] = sheet.getDataRange().getDisplayValues();
